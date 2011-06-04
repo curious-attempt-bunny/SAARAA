4a8e6db7d9c63bdbb3ae4faf61a9db0e8150de38
Merlyn Albery-Speyer <curious.attempt.bunny@gmail.com>
2011-06-04 15:27:16 -0700

    fleshed out basic form

--- gen/com/rhok/saaraa/R.java
+++ gen/com/rhok/saaraa/R.java
@@ -15,10 +15,13 @@ public final class R {
     }
     public static final class id {
         public static final int category=0x7f050001;
-        public static final int fireCategory=0x7f050003;
-        public static final int submitButton=0x7f050004;
+        public static final int peopleTrapped=0x7f050004;
+        public static final int severityLevel=0x7f050003;
+        public static final int submitButton=0x7f050007;
         public static final int textView1=0x7f050000;
-        public static final int textView2=0x7f050002;
+        public static final int textView3=0x7f050002;
+        public static final int textView4=0x7f050005;
+        public static final int whatDoYouSee=0x7f050006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/rhok/saaraa/WelcomeActivity.java
+++ src/com/rhok/saaraa/WelcomeActivity.java
@@ -17,11 +17,11 @@ public class WelcomeActivity extends Activity {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.main);
 
-	    Spinner spinner = (Spinner) findViewById(R.id.category);
-	    ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, GENRES);
-	    spinner.setAdapter(adapter);
+	    Spinner categories = initSpinner(R.id.category, CATEGORIES);
+	    //Spinner fireCategories = initSpinner(R.id.fireCategory, FIRE_CATEGORIES);
+	    initSpinner(R.id.severityLevel, SEVERITY_LEVEL);
 	    
-	    spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+	    categories.setOnItemSelectedListener(new OnItemSelectedListener() {
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 				String toast = "The category is " +
@@ -42,12 +42,26 @@ public class WelcomeActivity extends Activity {
 
 	}
 
+	private Spinner initSpinner(int id, String[] selections) {
+		Spinner spinner = (Spinner) findViewById(id);
+	    ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, selections);
+	    spinner.setAdapter(adapter);
+		return spinner;
+	}
+
     private void toast(View parent, String toast) {
 		Toast.makeText(parent.getContext(), toast, Toast.LENGTH_LONG).show();
 	}
 
-	private static final String[] GENRES = new String[] {
+	private static final String[] CATEGORIES = new String[] {
         "Fire", "Flood", "Gas Leak", "Down Wire", "Severe Weather", "Earthquake", "Other"
     };
 
+	private static final String[] FIRE_CATEGORIES = new String[] {
+        "Building", "Grass/Forest", "Vechicle", "Boat / Marina / Dock", "Other"
+    };
+	
+	private static final String[] SEVERITY_LEVEL = new String[] {
+        "Green", "Yellow", "Red"
+    };
 }
