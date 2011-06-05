2cf905eab9b9aa8c89b662d94b20e47c060f8ad3
bhouston <bhouston@yesmail.com>
2011-06-05 11:39:35 -0700

    photo capture now works!

--- gen/com/rhok/saaraa/R.java
+++ gen/com/rhok/saaraa/R.java
@@ -14,20 +14,19 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int addPhoto=0x7f05000a;
-        public static final int buttonClick=0x7f050002;
-        public static final int category=0x7f050004;
+        public static final int addPhoto=0x7f050009;
+        public static final int category=0x7f050003;
         public static final int layout=0x7f050000;
-        public static final int location=0x7f05000c;
-        public static final int peopleTrapped=0x7f050007;
+        public static final int location=0x7f05000b;
+        public static final int peopleTrapped=0x7f050006;
         public static final int preview=0x7f050001;
-        public static final int severityLevel=0x7f050006;
-        public static final int submitButton=0x7f05000d;
-        public static final int textLocation=0x7f05000b;
-        public static final int textView1=0x7f050003;
-        public static final int textView3=0x7f050005;
-        public static final int textView4=0x7f050008;
-        public static final int whatDoYouSee=0x7f050009;
+        public static final int severityLevel=0x7f050005;
+        public static final int submitButton=0x7f05000c;
+        public static final int textLocation=0x7f05000a;
+        public static final int textView1=0x7f050002;
+        public static final int textView3=0x7f050004;
+        public static final int textView4=0x7f050007;
+        public static final int whatDoYouSee=0x7f050008;
     }
     public static final class layout {
         public static final int capture=0x7f030000;
